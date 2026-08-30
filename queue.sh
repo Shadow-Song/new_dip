057df37002ab673283ceb6b5a895dbc5a8e2d568
Yiyuan Lu <ig25518@bristol.ac.uk>
2026-08-30 08:13:12 +0100

    removed hybrid

--- queue.sh
+++ queue.sh
@@ -31,6 +31,14 @@ echo "Started: $(date)"
 #     --ExecutePreprocessor.timeout=-1 \
 #     --ExecutePreprocessor.kernel_name=python3
 
-python main.py --function super-resolution
+# python main.py --function inpainting
+
+python run_hybrid.py \
+  --task inpainting \
+  --input data/inpainting/kate.png \
+  --mask data/inpainting/kate_mask.png \
+  --prompt "a realistic photograph of a woman" \
+  --dip-iters 3000 \
+  --strength 0.35
 
 echo "Finished: $(date)"
